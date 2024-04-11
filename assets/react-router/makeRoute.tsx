4203b27669f3eaff1af1dd5a246bab6e71386905
Jack Herrington <jherr@pobox.com>
2024-04-11 12:44:50 -0700

    Fixing react-routing in sub-links

--- assets/react-router/makeRoute.tsx
+++ assets/react-router/makeRoute.tsx
@@ -1,99 +1,99 @@
 /*
 Derived from: https://www.flightcontrol.dev/blog/fix-nextjs-routing-to-have-full-type-safety
 */
-import { ZodSchema, z } from 'zod'
-import queryString from 'query-string'
+import { ZodSchema, z } from "zod";
+import queryString from "query-string";
 import {
   Link,
   NavLink,
   useParams as useParmsRR,
   useSearchParams as useSearchParamsRR,
   RouteObject,
-} from 'react-router-dom'
+} from "react-router-dom";
 
-type LinkProps = Parameters<typeof Link>[0]
-type NavLinkProps = Parameters<typeof NavLink>[0]
+type LinkProps = Parameters<typeof Link>[0];
+type NavLinkProps = Parameters<typeof NavLink>[0];
 
 export type RouteInfo<
   Params extends z.ZodSchema,
-  Search extends z.ZodSchema,
+  Search extends z.ZodSchema
 > = {
-  name: string
-  params: Params
-  search: Search
-}
+  name: string;
+  params: Params;
+  search: Search;
+};
 
 export type RouteBuilder<
   Params extends z.ZodSchema,
-  Search extends z.ZodSchema,
+  Search extends z.ZodSchema
 > = {
-  (p?: z.input<Params>, search?: z.input<Search>): string
+  (p?: z.input<Params>, search?: z.input<Search>): string;
 
-  useParams: () => z.output<Params>
-  useSearchParams: () => z.output<Search>
-  useSetSearch: () => (search?: z.input<Search>) => void
+  useParams: () => z.output<Params>;
+  useSearchParams: () => z.output<Search>;
+  useSetSearch: () => (search?: z.input<Search>) => void;
 
   Link: React.FC<
-    Omit<LinkProps, 'to'> &
+    Omit<LinkProps, "to"> &
       z.input<Params> & {
-        search?: z.input<Search>
+        search?: z.input<Search>;
       } & { children?: React.ReactNode }
-  >
+  >;
   ParamsLink: React.FC<
-    Omit<LinkProps, 'to'> & {
-      params?: z.input<Params>
-      search?: z.input<Search>
+    Omit<LinkProps, "to"> & {
+      params?: z.input<Params>;
+      search?: z.input<Search>;
     } & { children?: React.ReactNode }
-  >
+  >;
   NavLink: React.FC<
-    Omit<NavLinkProps, 'to'> &
+    Omit<NavLinkProps, "to"> &
       z.input<Params> & {
-        search?: z.input<Search>
+        search?: z.input<Search>;
       } & { children?: React.ReactNode }
-  >
+  >;
   NavParamsLink: React.FC<
-    Omit<NavLinkProps, 'to'> & {
-      params?: z.input<Params>
-      search?: z.input<Search>
+    Omit<NavLinkProps, "to"> & {
+      params?: z.input<Params>;
+      search?: z.input<Search>;
     } & { children?: React.ReactNode }
-  >
+  >;
 
-  params: z.output<Params>
-  paramsSchema: Params
-  search: z.output<Search>
-  searchSchema: Search
-}
+  params: z.output<Params>;
+  paramsSchema: Params;
+  search: z.output<Search>;
+  searchSchema: Search;
+};
 
 function createPathBuilder<T extends Record<string, string | string[]>>(
-  route: string,
+  route: string
 ): (params: T) => string {
-  const pathArr = route.split('/')
+  const pathArr = route.split("/");
 
-  let catchAllSegment: ((params: T) => string) | null = null
-  if (pathArr.at(-1) === '*') {
-    const catchKey = '*'
+  let catchAllSegment: ((params: T) => string) | null = null;
+  if (pathArr.at(-1) === "*") {
+    const catchKey = "*";
     catchAllSegment = (params: T) => {
-      const catchAll = params[catchKey] as unknown as string[]
-      return catchAll ? `/${catchAll.join('/')}` : ''
-    }
+      const catchAll = params[catchKey] as unknown as string[];
+      return catchAll ? `/${catchAll.join("/")}` : "";
+    };
   }
 
-  const elems: ((params: T) => string | null)[] = []
+  const elems: ((params: T) => string | null)[] = [];
   for (const elem of pathArr) {
-    if (elem.endsWith('?')) {
-      const key = elem.replace('?', '')
+    if (elem.endsWith("?")) {
+      const key = elem.replace("?", "");
       elems.push((params: T) => {
         if (params[key]) {
-          return params[key as unknown as string] as string
+          return params[key as unknown as string] as string;
         } else {
-          return null
+          return null;
         }
-      })
-    } else if (elem.startsWith(':')) {
-      const key = elem.replace(':', '')
-      elems.push((params: T) => params[key as unknown as string] as string)
+      });
+    } else if (elem.startsWith(":")) {
+      const key = elem.replace(":", "");
+      elems.push((params: T) => params[key as unknown as string] as string);
     } else {
-      elems.push(() => elem)
+      elems.push(() => elem);
     }
   }
 
@@ -101,118 +101,121 @@ function createPathBuilder<T extends Record<string, string | string[]>>(
     const p = elems
       .map((e) => e(params))
       .filter((v) => v)
-      .join('/')
+      .join("/");
+
     if (catchAllSegment) {
-      return p + catchAllSegment(params)
+      return "/" + p + catchAllSegment(params);
     } else {
-      return p
+      return "/" + p;
     }
-  }
+  };
 }
 
-const emptySchema = z.object({})
+const emptySchema = z.object({});
 
 export function makeRoute<
   Params extends z.ZodSchema,
-  Search extends z.ZodSchema = typeof emptySchema,
+  Search extends z.ZodSchema = typeof emptySchema
 >(
   route: string,
-  info: RouteInfo<Params, Search>,
+  info: RouteInfo<Params, Search>
 ): RouteBuilder<Params, Search> {
-  const fn = createPathBuilder<z.output<Params>>(route)
+  const fn = createPathBuilder<z.output<Params>>(route);
 
   const routeBuilder: RouteBuilder<Params, Search> = (
     params?: z.input<Params>,
-    search?: z.input<Search>,
+    search?: z.input<Search>
   ) => {
-    let checkedParams = params || {}
+    let checkedParams = params || {};
     if (info.params) {
-      const safeParams = info.params.safeParse(checkedParams)
+      const safeParams = info.params.safeParse(checkedParams);
       if (!safeParams?.success) {
         throw new Error(
-          `Invalid params for route ${info.name}: ${safeParams.error.message}`,
-        )
+          `Invalid params for route ${info.name}: ${safeParams.error.message}`
+        );
       } else {
-        checkedParams = safeParams.data
+        checkedParams = safeParams.data;
       }
     }
-    const safeSearch = info.search ? info.search?.safeParse(search || {}) : null
+    const safeSearch = info.search
+      ? info.search?.safeParse(search || {})
+      : null;
     if (info.search && !safeSearch?.success) {
       throw new Error(
-        `Invalid search params for route ${info.name}: ${safeSearch?.error.message}`,
-      )
+        `Invalid search params for route ${info.name}: ${safeSearch?.error.message}`
+      );
     }
 
-    const baseUrl = fn(checkedParams)
-    const searchString = search && queryString.stringify(search)
-    return [baseUrl, searchString ? `?${searchString}` : ''].join('')
-  }
+    const baseUrl = fn(checkedParams);
+    const searchString = search && queryString.stringify(search);
+    return [baseUrl, searchString ? `?${searchString}` : ""].join("");
+  };
 
   routeBuilder.useParams = function useParams(): z.output<Params> {
-    const res = info.params.safeParse(useParmsRR())
+    const res = info.params.safeParse(useParmsRR());
     if (!res.success) {
       throw new Error(
-        `Invalid route params for route ${info.name}: ${res.error.message}`,
-      )
+        `Invalid route params for route ${info.name}: ${res.error.message}`
+      );
     }
-    return res.data
-  }
+    return res.data;
+  };
 
   if (info?.search) {
     routeBuilder.useSearchParams =
       function useSearchParams(): z.output<Search> {
-        const [searchParams] = useSearchParamsRR()
+        const [searchParams] = useSearchParamsRR();
         const res = info.search!.safeParse(
-          convertURLSearchParamsToObject(searchParams),
-        )
+          convertURLSearchParamsToObject(searchParams)
+        );
         if (!res.success) {
           throw new Error(
-            `Invalid search params for route ${info.name}: ${res.error.message}`,
-          )
+            `Invalid search params for route ${info.name}: ${res.error.message}`
+          );
         }
-        return res.data
-      }
+        return res.data;
+      };
   } else {
     routeBuilder.useSearchParams = function useSearchParams() {
-      throw new Error(`Route ${info.name} does not have search params`)
-    }
+      throw new Error(`Route ${info.name} does not have search params`);
+    };
   }
 
   routeBuilder.useSetSearch = function useSetSearch() {
-    const [_, setSearch] = useSearchParamsRR()
+    const [_, setSearch] = useSearchParamsRR();
     return (search?: z.input<Search>) => {
-      setSearch(search)
-    }
-  }
+      setSearch(search);
+    };
+  };
 
   routeBuilder.ParamsLink = function RouteLink({
     params: linkParams,
     search: linkSearch,
     children,
     ...props
-  }: Omit<LinkProps, 'to'> & {
-    params?: z.input<Params>
-    search?: z.input<Search>
+  }: Omit<LinkProps, "to"> & {
+    params?: z.input<Params>;
+    search?: z.input<Search>;
   } & { children?: React.ReactNode }) {
     return (
       <Link {...props} to={routeBuilder(linkParams, linkSearch)}>
         {children}
       </Link>
-    )
-  }
+    );
+  };
 
   routeBuilder.Link = function RouteLink({
     search: linkSearch,
     children,
     ...props
-  }: Omit<LinkProps, 'to'> &
+  }: Omit<LinkProps, "to"> &
     z.input<Params> & {
-      search?: z.input<Search>
+      search?: z.input<Search>;
     } & { children?: React.ReactNode }) {
-    const params = info.params.parse(props)
-    const extraProps = { ...props }
+    const params = info.params.parse(props);
+    const extraProps = { ...props };
     for (const key of Object.keys(params)) {
-      delete extraProps[key]
+      delete extraProps[key];
     }
     return (
       <Link
@@ -221,37 +224,37 @@ export function makeRoute<
       >
         {children}
       </Link>
-    )
-  }
+    );
+  };
 
   routeBuilder.NavParamsLink = function RouteLink({
     params: linkParams,
     search: linkSearch,
     children,
     ...props
-  }: Omit<NavLinkProps, 'to'> & {
-    params?: z.input<Params>
-    search?: z.input<Search>
+  }: Omit<NavLinkProps, "to"> & {
+    params?: z.input<Params>;
+    search?: z.input<Search>;
   } & { children?: React.ReactNode }) {
     return (
       <NavLink {...props} to={routeBuilder(linkParams, linkSearch)}>
         {children}
       </NavLink>
-    )
-  }
+    );
+  };
 
   routeBuilder.NavLink = function RouteLink({
     search: linkSearch,
     children,
     ...props
-  }: Omit<NavLinkProps, 'to'> &
+  }: Omit<NavLinkProps, "to"> &
     z.input<Params> & {
-      search?: z.input<Search>
+      search?: z.input<Search>;
     } & { children?: React.ReactNode }) {
-    const params = info.params.parse(props)
-    const extraProps = { ...props }
+    const params = info.params.parse(props);
+    const extraProps = { ...props };
     for (const key of Object.keys(params)) {
-      delete extraProps[key]
+      delete extraProps[key];
     }
     return (
       <NavLink
@@ -260,88 +263,88 @@ export function makeRoute<
       >
         {children}
       </NavLink>
-    )
-  }
+    );
+  };
 
-  routeBuilder.params = undefined as z.output<Params>
-  routeBuilder.paramsSchema = info.params
-  routeBuilder.search = undefined as z.output<Search>
-  routeBuilder.searchSchema = info.search
+  routeBuilder.params = undefined as z.output<Params>;
+  routeBuilder.paramsSchema = info.params;
+  routeBuilder.search = undefined as z.output<Search>;
+  routeBuilder.searchSchema = info.search;
 
-  return routeBuilder
+  return routeBuilder;
 }
 
 function convertURLSearchParamsToObject(
-  params: Readonly<URLSearchParams> | null,
+  params: Readonly<URLSearchParams> | null
 ): Record<string, string | string[]> {
   if (!params) {
-    return {}
+    return {};
   }
 
-  const obj: Record<string, string | string[]> = {}
+  const obj: Record<string, string | string[]> = {};
   for (const [key, value] of params.entries()) {
     if (params.getAll(key).length > 1) {
-      obj[key] = params.getAll(key)
+      obj[key] = params.getAll(key);
     } else {
-      obj[key] = value
+      obj[key] = value;
     }
   }
-  return obj
+  return obj;
 }
 
 // Super hairy TypeScript generic code to convert a route tree into a map of route builders
 
-type RouteNode = Omit<RouteObject, 'children'> & {
-  name: string
-  params: ZodSchema
-  search: ZodSchema
-  children?: RouteNode[]
-}
+type RouteNode = Omit<RouteObject, "children"> & {
+  name: string;
+  params: ZodSchema;
+  search: ZodSchema;
+  children?: RouteNode[];
+};
 
-type RouteNodeInfer<S extends string> = Omit<RouteObject, 'children'> & {
-  name: S
-  params: ZodSchema
-  search: ZodSchema
-  children?: RouteNodeInfer<S>[] | []
-}
+type RouteNodeInfer<S extends string> = Omit<RouteObject, "children"> & {
+  name: S;
+  params: ZodSchema;
+  search: ZodSchema;
+  children?: RouteNodeInfer<S>[] | [];
+};
 
 type RouteNodeArrToMap<T extends RouteNode[]> = {
-  [I in keyof T]: (x: RouteNodeToMap<T[I]>) => void
+  [I in keyof T]: (x: RouteNodeToMap<T[I]>) => void;
 }[number] extends (x: infer I) => void
   ? { [K in keyof I]: I[K] }
-  : never
+  : never;
 
 type RouteNodeToMap<T extends RouteNode> = T extends {
-  children: infer R extends RouteNode[]
+  children: infer R extends RouteNode[];
 }
   ? {
-      [P in T['name']]: RouteBuilder<T['params'], T['search']>
+      [P in T["name"]]: RouteBuilder<T["params"], T["search"]>;
     } & RouteNodeArrToMap<R>
-  : { [P in T['name']]: RouteBuilder<T['params'], T['search']> }
+  : { [P in T["name"]]: RouteBuilder<T["params"], T["search"]> };
 
 export function parseRoutes<T extends RouteNodeInfer<S>[], S extends string>(
-  routes: [...T],
+  routes: [...T]
 ): {
-  routes: RouteObject[]
-  declarativeRoutes: RouteNodeArrToMap<T>
+  routes: RouteObject[];
+  declarativeRoutes: RouteNodeArrToMap<T>;
 } {
-  const declarativeRoutes = {} as RouteNodeArrToMap<T>
+  const declarativeRoutes = {} as RouteNodeArrToMap<T>;
 
   function buildRoutes(
     routes: RouteNode[],
-    pathStack: (string | undefined)[],
+    pathStack: (string | undefined)[]
   ): RouteObject[] {
     function makePath(pathStack: (string | undefined)[]) {
       const path = pathStack
         .filter((x) => x)
-        .map((x) => (x === '/' ? '' : x))
-        .join('/')
-      return path
+        .map((x) => (x === "/" ? "" : x))
+        .join("/");
+      return path;
     }
 
     // @ts-ignore
     return routes.map((route) => {
-      const { name, params, search, ...rest } = route
+      const { name, params, search, ...rest } = route;
       // @ts-ignore
       declarativeRoutes[route.name as unknown as keyof RouteNodeArrToMap<T>] =
         makeRoute<typeof route.params, typeof route.search>(
@@ -350,22 +353,22 @@ export function parseRoutes<T extends RouteNodeInfer<S>[], S extends string>(
             name,
             params,
             search,
-          },
-        ) as unknown as keyof RouteNodeArrToMap<T>
+          }
+        ) as unknown as keyof RouteNodeArrToMap<T>;
       if (rest.children) {
         return {
           ...rest,
           children: buildRoutes(rest.children, [...pathStack, rest.path]),
-        }
+        };
       }
-      return rest
-    })
+      return rest;
+    });
   }
 
-  const simplifiedRoutes = buildRoutes(routes, [])
+  const simplifiedRoutes = buildRoutes(routes, []);
 
   return {
     routes: simplifiedRoutes,
     declarativeRoutes,
-  }
+  };
 }
